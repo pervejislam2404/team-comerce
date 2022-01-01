ab5f7986aa1b0ef1203fc484923f469560ab2843
pervejislam2404 <pervejislam2404@gmail.com>
2022-01-01 10:18:14 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>this is from pervej islam</h1>
+      <Button variant="info">testing bootstrap button</Button>
     </div>
   );
 }
