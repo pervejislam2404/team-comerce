4fddc144416ac446669cf3e151cb62b07b35808d
pervejislam2404 <pervejislam2404@gmail.com>
2022-01-01 22:42:06 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Home from "./pages/Home/Home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Header from "./pages/Shared/Header/Header";
 
 function App() {
   return (
     <div>
-      <Home />
+
+      <Router>
+        <Header/>
+        <Routes>
+        <Route path="/" element={<Home />}/>
+        </Routes>        
+      </Router>
+      
     </div>
   );
 }
