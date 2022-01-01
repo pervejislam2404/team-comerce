7b653f6b3644624b7018da7368268c40577dc680
pervejislam2404 <pervejislam2404@gmail.com>
2022-01-01 20:02:42 +0600

    update

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,12 +2,15 @@ import React from "react";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
-  return (
-    <div>
-      <Banner />
-      <h2>Siam</h2>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        Banner / >
+        <
+        h2 > Siam < /h2> <
+        h2 > pervej < /h2> <
+        /div>
+    );
 };
 
 export default Home;
