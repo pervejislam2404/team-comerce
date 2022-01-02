f617fb26a81f7bbc6fb1589dc8c163f737ec8180
Sifat <ibnsifat@gmail.com>
2022-01-02 14:21:14 +0600

    admin links

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, ListGroup, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Dashboard = () => {
     // for now
@@ -12,6 +13,20 @@ const Dashboard = () => {
                     <div className="sidebar-heading text-dark text-center py-4 primary-text fs-4 fw-bold text-uppercase">
                         <i className="fas fa-user-secret"></i>{admin ? "admin" : "userName"}
                     </div>
+                    <ListGroup variant="flush" className='me-3'>
+                        {/* user routes*/}
+                        {!admin && <div>
+                            <NavLink to={`/dashboard`}><ListGroup.Item className=' border-end-0 border-top-0 border-start-0'><i class="fas fa-home"></i> Home</ListGroup.Item></NavLink>
+                        </div>}
+                        {/* admin routes*/}
+                        {admin && <div>
+
+                            <NavLink to={`/dashboard/admin`}><ListGroup.Item className=' border-end-0 border-top-0 border-start-0'><i className="fas fa-user-lock"></i> Add Admin</ListGroup.Item></NavLink>
+                            <NavLink to={`/dashboard/allUser`}><ListGroup.Item className=' border-end-0 border-top-0 border-start-0'><i className="fas fa-biking"></i> All users </ListGroup.Item></NavLink>
+                            <NavLink to={`/dashboard/allOrder`}><ListGroup.Item className='border-end-0 border-top-0 border-start-0'><i className="fas fa-baby"></i> Order List</ListGroup.Item></NavLink>
+                        </div>}
+                        <ListGroup.Item onClick='{logOut}' className='point '><i class="fas fa-sign-out-alt"></i> LogOut</ListGroup.Item>
+                    </ListGroup>
                 </Container>
             </Row>
         </Container>
