9c736cd014cb230d8f89aface712957f3160e5ab
Sifat <ibnsifat@gmail.com>
2022-01-02 14:15:32 +0600

    dashbaord show name

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
 
 const Dashboard = () => {
+    // for now
+    const [admin, setadmin] = useState(true)
+
     return (
-        <div>
-            this is dashboard
-        </div>
+        <Container fluid className=' prim-bg d-flex' id="wrapper">
+            <Row id="sidebar-wrapper">
+                <Container className='text-center bg-white  sw'>
+                    <div className="sidebar-heading text-dark text-center py-4 primary-text fs-4 fw-bold text-uppercase">
+                        <i className="fas fa-user-secret"></i>{admin ? "admin" : "userName"}
+                    </div>
+                </Container>
+            </Row>
+        </Container>
     );
 };
 
