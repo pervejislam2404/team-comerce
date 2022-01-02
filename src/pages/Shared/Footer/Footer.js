723260c0b24d7d46f1c5309c3935bcd294b1981f
Sifat <ibnsifat@gmail.com>
2022-01-02 13:37:54 +0600

    footer started

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { Col, Container, Row } from 'react-bootstrap';
+import footImg from './footImg.gif'
 const Footer = () => {
     return (
-        <div>
-           <h1 className="text-danger fw-bold"> this is footer</h1>
-        </div>
+        <Container fluid className='footer  ps-5'>
+            <Row className='mt-5 pt-5'>
+                {/*  footer img */}
+                <Col xs={12} md={4} className='social ps-5'>
+                    <img src={footImg} alt="" height='100' width='100' />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
