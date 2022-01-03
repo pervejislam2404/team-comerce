f366af5d227997a5281c9c0f58f2efca2a776126
Sifat <ibnsifat@gmail.com>
2022-01-04 02:41:44 +0600

    product search by category at home

--- src/pages/Home/HomeProducts/HomeProducts.js
+++ src/pages/Home/HomeProducts/HomeProducts.js
@@ -4,13 +4,41 @@ import HomeProduct from "./HomeProduct";
 
 const HomeProducts = () => {
   const [products, setProducts] = useState([]);
+  const [display, setDisplay] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://limitless-hollows-74908.herokuapp.com/getProductByCategory/shoes"
-    )
+    fetch("https://limitless-hollows-74908.herokuapp.com/getAllProducts")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(() => {
+    setDisplay(products);
+    console.log(products);
+  }, [products]);
+  const menClick = async () => {
+    console.log('click3');
+
+    let result = await products.filter(product => product.gender === 'men')
+    setDisplay(result);
+  }
+  const womenClick = async () => {
+    console.log('click2');
+
+    let result = await products.filter(product => product.gender === 'women')
+    setDisplay(result);
+  }
+  const allClick = async () => {
+    console.log('click1');
+    let result = await products.filter(product => product.gender === 'all')
+    setDisplay(result);
+  }
+
+  // useEffect(() => {
+  //   fetch(
+  //     "https://limitless-hollows-74908.herokuapp.com/getProductByCategory/shoes"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data));
+  // }, []);
   return (
     <div className="container py-5">
       <div className="d-flex justify-content-center pt-5">
@@ -20,13 +48,13 @@ const HomeProducts = () => {
         </div>
       </div>
       <div className="d-flex justify-content-center pb-5 pt-3">
-        <h5 className="px-3">FOR ALL</h5>
-        <h5 className="px-3">FOR MEN</h5>
-        <h5 className="px-3">FOR WOMEN</h5>
+        <h5 onClick={allClick} className="px-3">FOR ALL</h5>
+        <h5 onClick={menClick} className="px-3">FOR MEN</h5>
+        <h5 onClick={womenClick} className="px-3">FOR WOMEN</h5>
       </div>
 
       <div class="row">
-        {products?.map((product) => (
+        {display?.map((product) => (
           <HomeProduct product={product} />
         ))}
       </div>
