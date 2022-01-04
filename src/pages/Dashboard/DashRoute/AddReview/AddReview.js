c57fe68938a7a6e0fd1058c330b3243d70f74c76
Sifat <ibnsifat@gmail.com>
2022-01-04 23:58:16 +0600

    review form done

--- src/pages/Dashboard/DashRoute/AddReview/AddReview.js
+++ src/pages/Dashboard/DashRoute/AddReview/AddReview.js
@@ -10,10 +10,28 @@ const AddReview = () => {
     const handleShow = () => setShow(true);
     const user = useSelector((state) => state.statesCounter.user);
 
-    return (
-        <div>
+    // sending review data to db
+    const onSubmit = data => {
+        reset('');
+        console.log(data)
+        axios.post('https://limitless-hollows-74908.herokuapp.com/saveUserReview', data).then(res => res.data ? handleShow() : '')
 
-        </div>
+    }
+    console.log(user.photoURL);
+    return (
+        // review data form
+        <Container data-aos="zoom-in-up">
+            <h2 className='text-center'>Give Us a Review</h2>
+            <hr className='w-25 mx-auto' />
+            <form className='mt-3 mx-auto pt-2 w-50' onSubmit={handleSubmit(onSubmit)}>
+                <input required placeholder='name' defaultValue={user.displayName} className='reservation w-100' {...register("name")} />
+                <input required placeholder='image' defaultValue={user.photoURL} className='reservation w-100'{...register("img")} />
+                <textarea rows='4' required placeholder='description' className='reservation w-100'{...register("descript")} />
+                <input type='number' min='1' max='5' required placeholder='rating' className='reservation w-100'{...register("star")} />
+                <button className='adminAdd btncolr w-25 mx-auto mt-4 fs-3 '>Add </button>
+
+            </form>
+        </Container>
     );
 };
 
